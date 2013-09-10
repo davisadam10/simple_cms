53b1058e8ed89bb520d40392d8faf4a704acb555
Adam Davis <davisadam10@googlemail.com>
2013-09-10 16:17:38 +0100

    Correct the file path

--- tests/test_firstTest.py
+++ tests/test_firstTest.py
@@ -3,13 +3,12 @@ from nose.tools import *
 
 _globals = {"filePath": None}
 def my_setup():
-	_globals["filePath"] = "/home/adam-d/"
+	_globals["filePath"] = "/home/adam/"
 
 def my_teardown():
 	_globals["filePath"] = None
 
 @with_setup(my_setup, my_teardown)
 def test_filePath():
-	print _globals["filePath"]
 	if not os.path.exists(_globals["filePath"]):
 		assert False
